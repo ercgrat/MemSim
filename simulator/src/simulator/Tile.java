c1eb20ad1657366b7348422d09276370f69b585e
Ubuntu <ubuntu@ip-172-31-3-103.ec2.internal>
2015-11-06 02:55:33 +0000

    Created Directory classes.

--- simulator/src/simulator/Tile.java
+++ simulator/src/simulator/Tile.java
@@ -5,9 +5,13 @@ import java.util.*;
 public class Tile {
 	
 	private ArrayList<Access> ops;
+	private Directory L1;
+	public L2Directory L2;
 
 	public Tile() {
 		ops = new ArrayList<Access>();
+		L1 = new Directory();
+		L2 = new L2Directory(0); // fill in with p later
 	}
 	
 	public void addAccess(Access access) {
